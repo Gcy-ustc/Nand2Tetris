d5907c685e5b9470444e36a0599401261a6bad6a
Gcy-ustc <ustcgcy@Phantom-2.local>
2017-05-17 19:36:23 +0800

    Finish Project 4

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -6,9 +6,54 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+
+(LOOP)
+    @KBD
+    D=M // get keyboard value
+    @CLEAN
+    D;JEQ
+    (WRITE)
+        @i
+        M=0
+        (LOOP1)
+            @i
+            D=M
+            @8192
+            D=D-A
+            @LOOP
+            D;JGE // if i > 8192 goto LOOP
+            @i
+            D=M
+            @SCREEN
+            A=A+D
+            M=-1
+            @i
+            M=M+1
+            @LOOP1
+            0;JMP
+    (CLEAN)
+        @i
+        M=0
+        (LOOP2)
+            @i
+            D=M
+            @8192
+            D=D-A
+            @LOOP
+            D;JGE // if i > 8192 goto LOOP
+            @i
+            D=M
+            @SCREEN
+            A=A+D
+            M=0
+            @i
+            M=M+1
+            @LOOP2
+            0;JMP
